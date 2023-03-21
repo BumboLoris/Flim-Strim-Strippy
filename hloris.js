7e354038d288463215836634954dd89fff2a3e61
Borton Fweems <borton.fweems@treadleandloam.com>
2023-03-21 10:58:07 -0700

    oh, well, tidy things up a bit...

--- hloris.js
+++ hloris.js
@@ -7,62 +7,44 @@ for (let q = 0  ;  q < num_im  ;  ++q)
   strim . push (new Image ());
 
 
-let im;
-
 for (let q = 0  ;  q < num_im  ;  ++q)
-  { im = strim[q];
+  { let im = strim[q];
     im.src = "image" + (q+1) + ".png";
     im.style.borderWidth = "0px";
     im.style.borderStyle = "solid";
-/*
-    if (q == 2)
-      im.style.borderColor = "red";
-    else
-      im.style.borderColor = "black";
-*/
-    im.namai = "" + (q+1);
+
+    im.namai = "immy-" + (q+1);  // just a li'l name for debugging.
 
     im . addEventListener ("mouseenter",
                            function (ev) { this.style.borderColor = "red";
-//                                           console.log ("IN -- " + this.namai);
-                                              this.style.borderWidth = "thick";
-
-                                              this.focus ();
+                                           this.style.borderWidth = "thick";
+                                           this.focus ();
                                          }
                           );
     im . addEventListener ("mouseleave",
                            function (ev) { this.style.borderColor = "black";
                                             this.style.borderWidth = "0px";
                                             this.blur ();
-//                                           console.log ("OUT -- " + this.namai);
                                          }
                           );
 
-im . addEventListener ("mousedown",
+    im . addEventListener ("mousedown",
                            function (ev) { this.style.borderColor = "blue";
-                                            this.style.borderWidth = "100px";
-
-//                                           console.log ("OUT -- " + this.namai);
+                                           this.style.borderWidth = "100px";
                                          }
                           );
 
-im . addEventListener ("mouseup",
+    im . addEventListener ("mouseup",
                            function (ev) { this.style.borderColor = "pink";
-                                            this.style.borderWidth = "0px";
-                                            this.before("frog");
-//                                           console.log ("OUT -- " + this.namai);
+                                           this.style.borderWidth = "0px";
+                                           this.before("frog");
                                          }
                           );
 
-im . addEventListener ("keydown",
-                           function (ev) { 
-                           //                  this.style.borderColor = "blue";
-                           //                  this.style.borderWidth = "100px";
-                                          console.log ("key -- " + this.namai);
-// let frimp=globalThis.document . createElement ('div');
-
-// frimp.textContent="frog";
-
+    im . addEventListener ("keydown",
+                           function (ev) { // this.style.borderColor = "blue";
+                                           // this.style.borderWidth = "100px";
+                                           console.log ("key -- " + this.namai);
                                          }
                           );
 }
@@ -86,9 +68,8 @@ strippy . appendChild (strim[num_im]);
 
 
 setInterval (function () { flim.src = strim[flimcount].src;
-                            if (++flimcount >= num_im)
-                              flimcount = 0;
+                           if (++flimcount  >=  num_im)
+                             flimcount = 0;
                          },
-                        333
+             333
             );
-
