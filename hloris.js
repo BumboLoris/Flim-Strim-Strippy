790cfad2506dc05178b232fa14403c6d97773e36
Borton Fweems <borton.fweems@treadleandloam.com>
2023-04-05 17:13:43 -0700

    more reorganization, cleanup, fiddling.

--- hloris.js
+++ hloris.js
@@ -1,12 +1,5 @@
 
-window.strim = new Array ();
-
 const num_im = 5;
-
-for (let q = 0  ;  q < num_im  ;  ++q)
-  strim . push (new Image ());
-
-let hoveree = null;
 
 function MouseEnterIm (ev)
 { this.style.borderColor = "red";
@@ -37,29 +30,75 @@ function KeyDownUniv (ev)
     hoveree . before (" flap");
 }
 
-for (let q = 0  ;  q < num_im  ;  ++q)
-  { let im = strim[q];
-    im.src = "image" + (q+1) + ".png";
-    im.style.borderWidth = "0px";
-    im.style.borderStyle = "solid";
 
-    im.namai = "immy-" + (q+1);  // just a li'l name for debugging.
+function CompletedIm (ev)
+{ //
+  console.log (ev);
+  this.style.width = "auto";
+  this.style.height = "70%";
+}
 
-    im . addEventListener ("mouseenter", MouseEnterIm);
-    im . addEventListener ("mouseleave", MouseLeaveIm);
-    im . addEventListener ("mousedown", MouseDownIm);
-    im . addEventListener ("mouseup", MouseUpIm);
+function BadloadIm (ev)
+{ //
+  console.log (ev);
 }
 
 
 let strippy = window.document . createElement ('div');
 let soloist = window.document . createElement ('div');
 
+window.strim = new Array ();  // attaches 'strim' to the global object
+
+let hoveree = null;
+
+let cusser = window.document . createElement ('div');
+let canvoo = window.document . createElement ('canvas');
+cusser . appendChild (canvoo);
+cusser.style.position = 'absolute';
+cusser.style.left = '100px';
+cusser.style.top = '200px';
+cusser.style.width = '60px';
+cusser.style.height = '60px';
+cusser["style"]["pointer-events"] = 'none';
+
+canvoo.style.width = "100%";
+canvoo.style.height = "100%";
+canvoo.style.backgroundColor = "#20ff2050";
+globalThis.canvoo = canvoo;
+
+function CursorShambler (ev)
+{ cusser.style.left = ""  +  (ev.clientX - 30)  +  "px";
+  cusser.style.top = ""  +  (ev.clientY - 30)  +  "px";
+}
+
+window.document . addEventListener ("mousemove", CursorShambler);
+
 window.document.body . appendChild (strippy);
 window.document.body . appendChild (soloist);
 
+window.document.body . appendChild (cusser);
+
+strippy.style.height = "10%";
+
 for (let q = 0  ;  q < num_im  ;  ++q)
-  strippy . appendChild (strim[q]);
+  { let im = new Image ();
+    strim . push (im);
+    strippy . appendChild (im);
+    im.namai = "immy-" + (q+1);  // just a li'l name for debugging.
+
+    im.style.borderWidth = "0px";
+    im.style.borderStyle = "solid";
+
+    im . addEventListener ("mouseenter", MouseEnterIm);
+    im . addEventListener ("mouseleave", MouseLeaveIm);
+    im . addEventListener ("mousedown", MouseDownIm);
+    im . addEventListener ("mouseup", MouseUpIm);
+
+    im . addEventListener ("load", CompletedIm);
+    im . addEventListener ("error", BadloadIm);
+    im.src = "image" + (q+1) + ".png";
+}
+
 
 let flim = new Image ();
 let flimcount = 0;
