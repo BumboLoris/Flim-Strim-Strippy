d32088f74800ce54c45b83dc30976babef8fae8f
Borton Fweems <borton.fweems@treadleandloam.com>
2023-04-26 18:48:13 -0700

    oh boy: horiz-grid images; duplicate 'em in preparation for left-scroll.

--- hloris.js
+++ hloris.js
@@ -1,27 +1,28 @@
 
 const num_im = 5;
 
+const inter_gap = 0;  // that'd be in pixels, Chet.
+
+const brdr_wid = "2px";
+
 function MouseEnterIm (ev)
 { this.style.borderColor = "red";
-  this.style.borderWidth = "thick";
+  this.style.borderWidth = "2px";
   hoveree = this;
 }
 
 function MouseLeaveIm (ev)
-{ this.style.borderColor = "black";
-  this.style.borderWidth = "0px";
-  //this.blur ();
+{ this.style.borderColor = "rgba(0,0,0,0.0)";
   hoveree = null;
 }
 
 function MouseDownIm (ev)
 { this.style.borderColor = "blue";
-  this.style.borderWidth = "100px";
+//  this.style.borderWidth = "100px";
 }
 
 function MouseUpIm (ev)
 { this.style.borderColor = "pink";
-  this.style.borderWidth = "0px";
   this.before(" frog");
 }
 
@@ -31,16 +32,11 @@ function KeyDownUniv (ev)
 }
 
 
-function CompletedIm (ev)
-{ //
-  console.log (ev);
-  this.style.width = "auto";
-  this.style.height = "70%";
-}
-
-function BadloadIm (ev)
-{ //
-  console.log (ev);
+function LampreyHandlersOntoImage (im)
+{ im . addEventListener ("mouseenter", MouseEnterIm);
+  im . addEventListener ("mouseleave", MouseLeaveIm);
+  im . addEventListener ("mousedown", MouseDownIm);
+  im . addEventListener ("mouseup", MouseUpIm);
 }
 
 
@@ -48,6 +44,60 @@ let strippy = window.document . createElement ('div');
 let soloist = window.document . createElement ('div');
 
 window.strim = new Array ();  // attaches 'strim' to the global object
+
+let total_wid = 0;
+
+function OmniscienceAttained ()
+{ let grid_str = "";
+
+  for (let q = 0  ;  q < num_im  ;  ++q)
+    { let im = strim[q];
+      let w = im.width;
+      if (w > 0)
+        w += inter_gap;
+      total_wid += w;
+      grid_str = grid_str + w + "px ";
+    }
+
+  // now: how many times do we need to repeat?
+  let winwid = window.innerWidth;
+  let duper_count = 0;
+  if (total_wid > winwid)
+    duper_count = 1;
+  else
+    duper_count = 1 + Math.floor (winwid / total_wid);
+
+  let full_grid_str = grid_str;
+  for (let extry = 0  ;  extry < duper_count  ;  ++extry)
+    full_grid_str = full_grid_str + grid_str;
+  strippy.style["grid-template-columns"] = full_grid_str;
+
+  for (let extry = 0  ;  extry < duper_count  ;  ++extry)
+    for (let q = 0  ;  q < num_im  ;  ++q)
+      { let im = strim[q] . cloneNode ()
+        strippy . appendChild (im);
+        LampreyHandlersOntoImage (im);
+      }
+}
+
+let loaded_so_far = 0;
+
+function CompletedIm (ev)
+{ //
+  console.log (ev);
+  this.style.width = "auto";
+  this.style.height = "80px";
+  if (++loaded_so_far == num_im)
+    OmniscienceAttained ();
+}
+
+function BadloadIm (ev)
+{ //
+  console.log (ev);
+  if (++loaded_so_far == num_im)
+    OmniscienceAttained ();
+}
+
 
 let hoveree = null;
 
@@ -78,7 +128,21 @@ window.document.body . appendChild (soloist);
 
 window.document.body . appendChild (cusser);
 
-strippy.style.height = "10%";
+strippy.style.width = "100%";
+strippy.style.height = "auto";
+strippy.style.borderWidth = "2px";
+strippy.style.borderStyle = "solid";
+strippy.style.borderColor = "green";
+strippy.style.position = 'absolute';
+strippy.style.top = '0px';
+strippy.style.left = '0px';
+strippy.style.display = 'grid';
+//strippy.style["grid-template-columns"] = 'repeat(15, 150px)';
+//strippy.style["grid-template-rows"] = '120px';
+strippy.style["align-items"] = 'center';
+//strippy.style.gap = '10px';
+
+let ell = 0;
 
 for (let q = 0  ;  q < num_im  ;  ++q)
   { let im = new Image ();
@@ -86,13 +150,10 @@ for (let q = 0  ;  q < num_im  ;  ++q)
     strippy . appendChild (im);
     im.namai = "immy-" + (q+1);  // just a li'l name for debugging.
 
-    im.style.borderWidth = "0px";
+    im.style.borderWidth = brdr_wid;
     im.style.borderStyle = "solid";
 
-    im . addEventListener ("mouseenter", MouseEnterIm);
-    im . addEventListener ("mouseleave", MouseLeaveIm);
-    im . addEventListener ("mousedown", MouseDownIm);
-    im . addEventListener ("mouseup", MouseUpIm);
+    LampreyHandlersOntoImage (im);
 
     im . addEventListener ("load", CompletedIm);
     im . addEventListener ("error", BadloadIm);
@@ -109,6 +170,8 @@ function FlipToNextImage ()
 { flim.src = strim[flimcount].src;
   if (++flimcount  >=  num_im)
       flimcount = 0;
+  ell -= 10;
+//  strippy.style.left = "" + ell + "px";
 }
 
 setInterval (FlipToNextImage, 333);
